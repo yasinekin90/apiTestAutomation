bd23e2ce8060cb90148fdfd3a06d36397229d2bb
yasinbaba <ekinyasin90@gmail.com>
2022-11-04 15:07:52 +0300

    customerPojo dosyası eklendi

---
+++ src/test/java/Post/CustomerPOJO.java
@@ -0,0 +1,242 @@
+
+package Post;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class CustomerPOJO {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("lastName")
+    @Expose
+    private String lastName;
+    @SerializedName("firstName")
+    @Expose
+    private String firstName;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("company")
+    @Expose
+    private String company;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("address1")
+    @Expose
+    private String address1;
+    @SerializedName("address2")
+    @Expose
+    private Object address2;
+    @SerializedName("city")
+    @Expose
+    private String city;
+    @SerializedName("state")
+    @Expose
+    private String state;
+    @SerializedName("postalCode")
+    @Expose
+    private String postalCode;
+    @SerializedName("country")
+    @Expose
+    private String country;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public CustomerPOJO() {
+    }
+
+    /**
+     * 
+     * @param lastName
+     * @param firstName
+     * @param country
+     * @param phone
+     * @param address2
+     * @param city
+     * @param address1
+     * @param postalCode
+     * @param company
+     * @param id
+     * @param state
+     * @param email
+     */
+    public CustomerPOJO(int id, String lastName, String firstName, String email, String company, String phone, String address1, Object address2, String city, String state, String postalCode, String country) {
+        super();
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+        this.company = company;
+        this.phone = phone;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public Object getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(Object address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(CustomerPOJO.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(this.id);
+        sb.append(',');
+        sb.append("lastName");
+        sb.append('=');
+        sb.append(((this.lastName == null)?"<null>":this.lastName));
+        sb.append(',');
+        sb.append("firstName");
+        sb.append('=');
+        sb.append(((this.firstName == null)?"<null>":this.firstName));
+        sb.append(',');
+        sb.append("email");
+        sb.append('=');
+        sb.append(((this.email == null)?"<null>":this.email));
+        sb.append(',');
+        sb.append("company");
+        sb.append('=');
+        sb.append(((this.company == null)?"<null>":this.company));
+        sb.append(',');
+        sb.append("phone");
+        sb.append('=');
+        sb.append(((this.phone == null)?"<null>":this.phone));
+        sb.append(',');
+        sb.append("address1");
+        sb.append('=');
+        sb.append(((this.address1 == null)?"<null>":this.address1));
+        sb.append(',');
+        sb.append("address2");
+        sb.append('=');
+        sb.append(((this.address2 == null)?"<null>":this.address2));
+        sb.append(',');
+        sb.append("city");
+        sb.append('=');
+        sb.append(((this.city == null)?"<null>":this.city));
+        sb.append(',');
+        sb.append("state");
+        sb.append('=');
+        sb.append(((this.state == null)?"<null>":this.state));
+        sb.append(',');
+        sb.append("postalCode");
+        sb.append('=');
+        sb.append(((this.postalCode == null)?"<null>":this.postalCode));
+        sb.append(',');
+        sb.append("country");
+        sb.append('=');
+        sb.append(((this.country == null)?"<null>":this.country));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
